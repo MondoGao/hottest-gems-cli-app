39b99376094f5583bcc9b08461ea6359d1bc5114
Mondo <mondogao@gmail.com>
2017-02-02 22:49:05 +0800

    fix(hottest_gems.rb): fix declaration's order

--- lib/hottest_gems.rb
+++ lib/hottest_gems.rb
@@ -2,8 +2,8 @@ require 'pry'
 require 'open-uri'
 require 'nokogiri'
 
-require_relative 'hottest_gems/cli'
-require_relative 'hottest_gems/gem'
-
 module HottestGems
 end
+
+require_relative 'hottest_gems/cli'
+require_relative 'hottest_gems/gem'
